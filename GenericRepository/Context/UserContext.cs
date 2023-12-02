6d2180ee779f2cf24a40aec5c650f953b8a2cb5a
BRIGHT <wissen30@hotmail.com>
2023-12-02 14:18:55 +0300

    Add project files.

---
+++ GenericRepository/Context/UserContext.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace GenericRepository.Context
+{
+    public class UserContext:DbContext
+    {
+        public DbSet<User> Users{ get; set; }
+        public UserContext(DbContextOptions<UserContext> options):base(options)
+        {
+            
+        }
+    }
+    public class User
+    {
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Guid { get; set; }
+        public bool IsActivated { get; set; }
+    }
+}
---
+++ GenericRepository/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using GenericRepository.Context;
+using GenericRepository.Models;
+using GenericRepository.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GenericRepository.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IUserService _userService;
+        public HomeController(IUserService userservice)
+        {
+            _userService = userservice;
+
+        }
+        public IActionResult AddUser(User user) {
+        
+        
+            _userService.AddUser(user);
+            return Ok();
+
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+
+        }
+
+
+
+
+    }
+}
